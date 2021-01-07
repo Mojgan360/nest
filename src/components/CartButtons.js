4083926a7c779b39288d04c239a269173bc10258
Mojgan360 <mojgan360@gmail.com>
2021-01-07 17:55:29 +0100

    Added Hero for HomePage

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
+import { FaShoppingCart, FaUserPlus } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 const CartButtons = () => {
   return (
