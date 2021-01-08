3d44a5f1dafd0d3d6544a9af7e6012f360951455
Mojgan360 <mojgan360@gmail.com>
2021-01-08 01:56:01 +0100

    Format Price

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { formatPrice } from '../utils/helpers'
 
 const Product = ({ id, name, price, image }) => {
   return (
@@ -14,7 +15,7 @@ const Product = ({ id, name, price, image }) => {
       </div>
       <footer>
         <h5>{name}</h5>
-        <p>$ {price}</p>
+        <p>{formatPrice(price)}</p>
       </footer>
     </Wrapper>
   )
---
+++ src/utils/helpers.js
@@ -0,0 +1,6 @@
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat('sv-SV', {
+    style: 'currency',
+    currency: 'SEK',
+  }).format(number / 100)
+}
