273f59e195e1c52f0b4e0eac9c220ed9c3a854a1
Mojgan360 <mojgan360@gmail.com>
2021-01-14 22:28:24 +0100

    ProductsPage - GridView

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react'
 import FilterContext from '../context/filter/filterContext'
-
+import GridViews from './GridViews'
 const ProductsList = () => {
   const filterContext = useContext(FilterContext)
   const { filtered_products: products } = filterContext
   return (
     <div>
       <h1>ProductsList</h1>
+      <GridViews products={products}>GridViews</GridViews>
     </div>
   )
 }
