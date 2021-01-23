03b32206ccf1a6a82f7ada36e3ef2fc09eef8acb
Mojgan360 <mojgan360@gmail.com>
2021-01-23 18:19:53 +0100

    Auth0 provider

--- src/index.js
+++ src/index.js
@@ -6,18 +6,26 @@ import { ProductsProvider } from './context/products_context'
 import AlertState from './context/alert/AlertState'
 import FilterState from './context/filter/FilterState'
 import CartState from './context/cart/CartState'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ProductsProvider>
-      <FilterState>
-        <CartState>
-          <AlertState>
-            <App />
-          </AlertState>
-        </CartState>
-      </FilterState>
-    </ProductsProvider>
-  </React.StrictMode>,
+  <Auth0Provider
+    domain='mojgan360.eu.auth0.com'
+    clientId='Js6a4V4WGLHPqMKpm0kf6VQBa4qPqGI1'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <React.StrictMode>
+      <ProductsProvider>
+        <FilterState>
+          <CartState>
+            <AlertState>
+              <App />
+            </AlertState>
+          </CartState>
+        </FilterState>
+      </ProductsProvider>
+    </React.StrictMode>
+  </Auth0Provider>,
   document.getElementById('root')
 )
