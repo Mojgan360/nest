f126cc003c2cb92634443a52f85c211da6fdd24b
Mojgan360 <mojgan360@gmail.com>
2021-01-24 20:06:09 +0100

    netlify - clean-up

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { PageHero } from '../components'
 // import CartContext from '../context/cart/cartContext'
